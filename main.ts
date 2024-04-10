bd931e89b57ae7460cec5b1ff67af3871455d8c6
Eduardo <eduardo.mantovani@ithinkupc.com>
2024-04-10 22:54:31 +0200

    detail pages created and linked

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 import { writeFile } from "fs/promises";
-import { render } from "./render.js";
+import { render, createDetailPage } from "./render.js";
 import { loadNews } from "./news.js";
 
 
 const news = await loadNews('business');
 const html = render(news);
 await writeFile('index.html', html);
+
+
+for (let i = 0; i < news.length; i++) {
+const article = news[i];
+await createDetailPage(article, i); 
+}
--- render.ts
+++ render.ts
@@ -1,4 +1,5 @@
 import { Article } from "./news.js";
+import { writeFile } from "fs/promises";
 
 const head = (title: string) => `
 <head>
@@ -35,11 +36,23 @@ const head = (title: string) => `
 </head>`;
 
 
-const renderNews = (news: Array<Article>) => {
+const articleHead = (title: string) => `
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${title} - The New York Times</title>  
+  <style></style>
+</head>`;
+
+
+const renderNews = (news: Array<Article>, index: number) => {
   let html = "";
-  for (const article of news) {
-    html += `<div class="article">
-      <img src="${article.multimediaUrl}" />
+  for (let i = 0; i < news.length; i++) {
+    const article = news[i];
+    html += `<div class="article" id="article-${i}">
+      <a href="./news/${index}.html">  <img src="${article.multimediaUrl}" />
+      </a>
       <div class="data">
         <div class="name">${article.title}</div>
         <div class="email">${article.description}</div>
@@ -47,15 +60,40 @@ const renderNews = (news: Array<Article>) => {
     </div>`;
   }
   return html;
-}
+};
 
 
 export const render = (news: Array<Article>) => {
+  let html = "";
+  for (let i = 0; i < news.length; i++) {
+    const article = news[i];
+    html += renderNews([article], i); // Wrap article in an array
+  }
   return `
-<html>
-  ${head("News List")}
-  <body>
-    ${renderNews(news)}
-  </body>
-</html>`;
+  <html>
+    ${head("The New York Times")}
+    <body>
+      ${html}
+    </body>
+  </html>`;
 };
+
+
+const renderDetail = (article: Article) => {
+  return `
+  <html>
+    ${articleHead(article.title)}
+    <body>
+      <h1>${article.title}</h1>
+      <p>${article.description}</p>
+      <img src="${article.multimediaUrl}" />
+    </body>
+  </html>`;
+};
+
+
+export async function createDetailPage(article: Article, index: number) {
+  const detailHtml = await renderDetail(article);
+  const detailFilePath = `news/${index}.html`; 
+  await writeFile(detailFilePath, detailHtml);
+}
